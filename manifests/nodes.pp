3162d6253ded307fbf634719c9aefcdad2c9d536
leadel Ngome <leadel@softcom.local>
2015-05-10 22:18:37 +0200

    [nodes] add windows nodes to node configuration

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,4 +1,25 @@
 node 'puppet-client01.localdomain' {
     notify { "yeah i am present, Hostname = ${::fqdn}": }
-    include wget_client
+    include ::wget_client
+    include ::sysadmin
+    include ::people_user
+    include ::ops
+    include ::samba
+}
+
+node 'puppet-master01.localdomain' {
+    include ::wget_client
+    include ::people_user
+    include ::ops
+    include ::samba
+}
+
+# # my Windows PC
+node 'leadel-pc.hitronhub.home' {
+    notify { "Installed on Windows": message => "Woow Leadel Thank you" }
+    include ::windows_ops
+}
+
+node 'eth-windows-server01.localdomain' inherits 'leadel-pc.hitronhub.home' {
+
 }
